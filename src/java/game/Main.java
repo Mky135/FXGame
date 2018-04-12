439ac538567ce7c06077c6f2f6ae95719abff704
Miguel Flores <mky.flores@hotmail.com>
2018-04-12 07:51:13 -0500

    fixed issues with main screen

--- src/java/game/Main.java
+++ src/java/game/Main.java
@@ -1,12 +1,30 @@
 package game;
 
+import game.controller.MainController;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.awt.*;
+import java.io.IOException;
+
+import static game.controller.MainController.KEYBOARD_MOVEMENT_DELTA;
+import static game.controller.MainController.m_circle;
+import static game.controller.MainController.transition;
+
 
 public class Main extends Application
 {
@@ -19,7 +37,7 @@ public class Main extends Application
      * The scene to be displayed when the user first opens the game.
      * Contains Nodes that let the user input their name
      */
-    public static Scene startScene;
+    private static Scene startScene;
 
     /**
      * The scene to be displayed when the user is playing
@@ -28,26 +46,35 @@ public class Main extends Application
      */
     public static Scene mainScene;
 
-    public static Scene nameScreen;
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+//    private Circle circle;
+    public static Group group;
+
+    public Main() throws IOException {}
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         // Keep a reference to the window
         window = primaryStage;
 
         primaryStage.setTitle("Game");
 
         // Build all of the scenes
-        Parent mainRoot = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
         Parent startRoot = FXMLLoader.load(getClass().getResource("startingScreen.fxml"));
-//        Parent nameRoot = FXMLLoader.load(getClass().getResource("nameScreen.fxml"));
+        Parent mainRoot = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
 
-        startScene = new Scene(startRoot, 600, 400);
-        mainScene = new Scene(mainRoot);
-//        nameScreen = new Scene(nameRoot);
+//        circle  = MainController.circle;
+//        circle = createCircle();
+
+        group = new Group(m_circle, mainRoot);
+
+        startScene = new Scene(startRoot);
+        mainScene = new Scene(group,600, 400, Color.BLACK);
+
+        moveCircleOnKeyPress();
+        moveCircleOnMousePress();
 
         // Display the window
         primaryStage.setScene(startScene);
@@ -65,5 +92,40 @@ public class Main extends Application
 
     public static void main(String[] args) { launch(args); }
 
+    private void moveCircleOnKeyPress()
+    {
+        //TODO: Make it so it accelerates
+        mainScene.setOnKeyPressed(event -> {
+            switch(event.getCode())
+            {
+                case UP:
+                    m_circle.setCenterY(m_circle.getCenterY() - KEYBOARD_MOVEMENT_DELTA);
+                    break;
+                case RIGHT:
+                    m_circle.setCenterX(m_circle.getCenterX() + KEYBOARD_MOVEMENT_DELTA);
+                    break;
+                case DOWN:
+                    m_circle.setCenterY(m_circle.getCenterY() + KEYBOARD_MOVEMENT_DELTA);
+                    break;
+                case LEFT:
+                    m_circle.setCenterX(m_circle.getCenterX() - KEYBOARD_MOVEMENT_DELTA);
+                    break;
+            }
+        });
+    }
+
+    private void moveCircleOnMousePress()
+    {
+        mainScene.setOnMousePressed(event -> {
+            if (!event.isControlDown()) {
+                m_circle.setCenterX(event.getSceneX());
+                m_circle.setCenterY(event.getSceneY());
+            } else {
+                transition.setToX(event.getSceneX() - m_circle.getCenterX());
+                transition.setToY(event.getSceneY() - m_circle.getCenterY());
+                transition.playFromStart();
+            }
+        });
+    }
 
 }
--- src/java/game/controller/MainController.java
+++ src/java/game/controller/MainController.java
@@ -1,15 +1,67 @@
 package game.controller;
 
+import game.Main;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable
 {
+
+    public static Circle m_circle;
+
+    public static TranslateTransition transition;
+
+    @FXML
+    public Button butt;
+
+    public static final int KEYBOARD_MOVEMENT_DELTA = 10;
+    private static final Duration TRANSLATE_DURATION = Duration.seconds(0.25);
+
+    @FXML
+    private void click()
+    {
+        butt.setText("clicked");
+    }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        m_circle = createCircle();
+        transition = createTranslateTransition(m_circle);
+    }
 
+    private Circle createCircle() {
+        final Circle circle = new Circle(151.0F, 201.0F, 100.0F, Color.DODGERBLUE);
+        circle.setStroke(Color.DODGERBLUE);
+        circle.setStrokeType(StrokeType.INSIDE);
+        circle.setOpacity(0.5);
+        return circle;
+    }
+
+    private TranslateTransition createTranslateTransition(final Circle circle)
+    {
+        final TranslateTransition transition = new TranslateTransition(TRANSLATE_DURATION, circle);
+        transition.setOnFinished(t -> {
+            circle.setCenterX(circle.getTranslateX() + circle.getCenterX());
+            circle.setCenterY(circle.getTranslateY() + circle.getCenterY());
+            circle.setTranslateX(0);
+            circle.setTranslateY(0);
+        });
+        return transition;
     }
 }
